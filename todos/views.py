321d6eafafff52765615d8f27dfd23867a71147c
Ricardo <ricardodesaqua14@gmail.com>
2024-11-19 19:52:12 -0300

    breviamente resolucao da questao 2

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Todo
 
-# Create your views here.
+class TodoListView(ListView):
+    model = Todo
+    template_name = 'todo_list.html'
+    context_object_name = 'todo_list'
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadline', 'finished_at']
+    template_name = 'todo_form.html'
+    success_url = reverse_lazy('todo_list')
